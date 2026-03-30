57bef70dc835021842a65e6ed2d611b5cb1fbdf2
Arthur Borges <arthur.borges@fatec.sp.gov.br>
2026-03-30 10:43:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-function enviarMsg() {
-  const input = document.getElementById("msgInput");
-  const texto = input.value.trim();
-  if (texto === "") return;
-
-  const chat = document.getElementById("chatBody");
-
-  // mensagem do usuário
-  const msgUser = document.createElement("div");
-  msgUser.className = "msg user";
-  msgUser.textContent = texto;
-  chat.appendChild(msgUser);
-
-  // limpa input
-  input.value = "";
-
-  // scroll vai para baixo automaticamente
-  chat.scrollTop = chat.scrollHeight;
-
-  // resposta automática
-  setTimeout(() => {
-    const msgBot = document.createElement("div");
-    msgBot.className = "msg";
-    msgBot.textContent = "  Olá! Já estamos verificando.";
-    chat.appendChild(msgBot);
-    chat.scrollTop = chat.scrollHeight;
-  }, 600);
-}
+document.getElementById("logoutBtn").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
